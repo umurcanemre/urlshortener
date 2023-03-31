0c28206ce677ddd5ea13e50d2e704d096b8a596e
Umurcan <5785630+umurcanemre@users.noreply.github.com>
2023-03-31 23:29:14 +0200

    Initial commit. Dependencies added

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+TODO : 
+sonar
+test
+ integ tests
+ unit test
+ contract tests
+ assertj
+secret management
+check help.md before submit
---
+++ src/test/java/com/ue/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ue.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
