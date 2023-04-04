c9f8b9e40f8062d14e0f874eb8e06981772161be
Umurcan <5785630+umurcanemre@users.noreply.github.com>
2023-04-04 23:52:45 +0200

    Sonar issues recommendations cleared

--- src/test/java/com/ue/urlshortener/UrlShortenerApplicationTests.java
+++ src/test/java/com/ue/urlshortener/UrlShortenerApplicationTests.java
@@ -1,13 +1,17 @@
 package com.ue.urlshortener;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class UrlShortenerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads(ApplicationContext context) {
+        assertThat(context).isNotNull();
+    }
 
 }
